a96ae453035ff9a6d01f8bf84c9ef52bf15e9a60
Julie Chajes <78927901+Juliechajes@users.noreply.github.com>
2021-04-25 21:15:11 +0300

    Update contact.js
    Added JS to build the sentence at the bottom of the page, so that it would appear on all three pages rather than just one.

--- js/contact.js
+++ js/contact.js
@@ -42,3 +42,22 @@ comment.addEventListener("input", onInputChange);
 male.addEventListener("click", onInputChange);
 female.addEventListener("click", onInputChange);
 other.addEventListener("click", onInputChange);
+
+const juliesArray = ["HTML", "CSS", "JavaScript"];
+
+const buildSentence = (languagesArray) => {
+  let intro = "This page was built using ";
+  for (let i = 0; i < languagesArray.length; i++) {
+    if (i === languagesArray.length - 1) {
+      intro = intro + "and " + languagesArray[i] + ".";
+    } else {
+      intro = intro + languagesArray[i] + ", ";
+    }
+  }
+
+  const changeFooterMessage = document.getElementById("footerMessage");
+  changeFooterMessage.textContent = intro;
+  return intro;
+};
+
+buildSentence(juliesArray);
