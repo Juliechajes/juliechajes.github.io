fe362898fc00cdcbaef07ef0b23decdc197d7194
Julie Chajes <78927901+Juliechajes@users.noreply.github.com>
2021-05-21 09:50:14 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-const juliesArray = ["HTML", "CSS", "JavaScript"];
-
-const buildSentence = (languagesArray) => {
-  let intro = "This page was built using ";
-  for (let i = 0; i < languagesArray.length; i++) {
-    if (i === languagesArray.length - 1) {
-      intro = intro + "and " + languagesArray[i] + ".";
-    } else {
-      intro = intro + languagesArray[i] + ", ";
-    }
-  }
-
-  const changeFooterMessage = document.getElementById("footerMessage");
-  changeFooterMessage.textContent = intro;
-  return intro;
-};
-
-buildSentence(juliesArray);
-
 const GITHUB_URL = "https://api.github.com/users/";
 const GITHUB_USERNAME = "Juliechajes";
 const githubInfo = GITHUB_URL.concat(GITHUB_USERNAME);
