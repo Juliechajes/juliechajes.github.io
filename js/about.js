2de83bdacf4317a8c46ff978b401269caeb1c312
Julie Chajes <78927901+Juliechajes@users.noreply.github.com>
2021-05-21 09:49:46 +0300

    Update about.js

--- js/about.js
+++ js/about.js
@@ -51,22 +51,3 @@ prevButton.addEventListener("click", changePrevMap);
 nextButton.addEventListener("click", changeNextMap);
 
 rotatingMap.src = mapsArray[i];
-
-const juliesArray = ["HTML", "CSS", "JavaScript"];
-
-const buildSentence = (languagesArray) => {
-  let intro = "This page was built using ";
-  for (let i = 0; i < languagesArray.length; i++) {
-    if (i === languagesArray.length - 1) {
-      intro = intro + "and " + languagesArray[i] + ".";
-    } else {
-      intro = intro + languagesArray[i] + ", ";
-    }
-  }
-
-  const changeFooterMessage = document.getElementById("footerMessage");
-  changeFooterMessage.textContent = intro;
-  return intro;
-};
-
-buildSentence(juliesArray);
